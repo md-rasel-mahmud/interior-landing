e0cadc0d994f47c0ee3157bbba2d22d45c79f314
md-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2025-10-24 01:52:00 +0600

    feat: add new category images and update fit-out and turnkey solutions in category list

--- src/constrain/category-list.tsx
+++ src/constrain/category-list.tsx
@@ -1,4 +1,6 @@
 import architectureImg from "@/assets/categories/01.png";
+import fitOutImg from "@/assets/categories/03.png";
+import turnkeySolutionImg from "@/assets/categories/03.png";
 export interface Category {
   slug: string;
   name: string;
@@ -21,13 +23,11 @@ export const categories: Category[] = [
   {
     slug: "fit-out",
     name: "Fit-out",
-    image:
-      "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?w=1200&h=800&fit=crop",
+    image: fitOutImg.src,
   },
   {
     slug: "turnkey-solutions",
     name: "Turnkey Solutions",
-    image:
-      "https://images.unsplash.com/photo-1497366216548-37526070297c?w=1200&h=800&fit=crop",
+    image: turnkeySolutionImg.src,
   },
 ];
