686a0e24efe7d03515ecc9cbbdd6db1a19afc263
md-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2025-12-01 21:31:13 +0600

    feat: Add PM2 ecosystem configuration for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "interior-landing",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start",
+      exec_mode: "cluster",
+      max_memory_restart: "1G",
+      autorestart: true,
+      instances: 2,
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
