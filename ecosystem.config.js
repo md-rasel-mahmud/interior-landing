eb38cd9122c38983470c6ce5e909c7031bded2bc
md-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2025-12-07 20:11:32 +0600

    [feat]: ecosystem change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,12 @@ module.exports = {
     {
       name: "interior-landing",
       script: "./node_modules/next/dist/bin/next",
-      args: "start",
-      exec_mode: "cluster",
-      max_memory_restart: "1G",
-      autorestart: true,
+      args: "start -p 3000",
+      exec_mode: "fork",
       instances: 1,
+      autorestart: true,
       watch: false,
+      max_memory_restart: "1500M",
       env: {
         NODE_ENV: "production",
         PORT: 3000,
